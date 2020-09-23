88d06181c137e05f2c170875bd31fef06f7cf627
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-09-23 11:29:55 -0700

    Renamed commands.py to cogs.py

--- teacherbot/bot.py
+++ teacherbot/bot.py
@@ -1,7 +1,7 @@
 import discord
 import datetime
 from discord.ext import commands, tasks
-from teacherbot.commands import MainCog
+from teacherbot.cogs import MainCog
 from teacherbot.database import session, Base, discord_users, student_data
 
 class bot(commands.Bot):
